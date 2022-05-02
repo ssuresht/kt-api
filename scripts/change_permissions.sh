dd4051a504c2f307647218d0a5857c6e7153fd1a
devinserito <dev7@inserito.me>
2022-05-02 15:00:20 +0530

    delete

--- scripts/change_permissions.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo usermod -a -G apache ubuntu
-sudo chown -R ubuntu:apache /var/www
-sudo chmod 2775 /var/www
-find /var/www -type d -exec sudo chmod 2775 {} \;
-find /var/www -type f -exec sudo chmod 0664 {} \;
