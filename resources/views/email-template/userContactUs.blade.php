e996ffa75636a02a9158ae3ecabb8d0429ee82fc
devinserito <dev7@inserito.me>
2022-05-03 18:07:37 +0530

    done some changes

--- resources/views/email-template/userContactUs.blade.php
+++ resources/views/email-template/userContactUs.blade.php
@@ -11,29 +11,29 @@
         以下の内容で送信いたしました。
     </p>
 
-    <br>
+    -----<br>
 
     <p>
         <span>会社名 / 学校名：{{ $content->company_or_school_name }}</span><br>
         <span>氏名：{{ $content->name }}</span><br>
         <span>メールアドレス：{{ $content->email }}</span><br>
          <span>電話番号：{{ $content->telephone }}</span><br>
-        <span>お問い合わせ内容：{!! nl2br(e($content->inquiry_content)) !!}</span><br>
+        <span>お問い合わせ内容：<br>{!! nl2br(e($content->inquiry_content)) !!}</span><br>
     </p>
 
     -----<br>
         <br>
     <p>
         2営業日以内に、担当者よりご連絡いたします。<br>
-        よろしくお願い致します
+        よろしくお願い致します。
     </p>
 
     <br>
     <br>
 
     <p>
-        本メールは、配信専用のアドレスで配信されています。
-        このメールにご返信いただいても、内容の確認およびご返答はできません。ご了承ください。
+        本メールは、配信専用のアドレスで配信されています。<br>
+        このメールにご返信いただいても、内容の確認およびご返答はできません。ご了承ください。<br>
         覚えがないのにこのメールを受け取られた方は、お手数ではございますがこのまま破棄をお願い致します。
     </p>
 
